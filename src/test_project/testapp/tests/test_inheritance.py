2166edb96623b60bfdf2d54eee341ce667c869dd
Enrico <rico.bl@gmail.com>
2012-08-16 15:59:10 -0300

    Added two-level inheritance test.

--- src/test_project/testapp/tests/test_inheritance.py
+++ src/test_project/testapp/tests/test_inheritance.py
@@ -4,9 +4,14 @@ from unittest import TestCase
 
 from test_project.helpers import compile_source
 
-class TestTemplateInheritance(TestCase):
+
+class TestSimpleTemplateInheritance(TestCase):
     
     def test_should_expand_one_level_inheritance(self):
         compiled = compile_source('{% extends "inheritance/level-one.html" %}').strip()
+        self.assertEqual(compiled, 'BASE TEMPLATE')
+
+    def test_should_expand_two_level_inheritance(self):
+        compiled = compile_source('{% extends "inheritance/level-two.html" %}').strip()
         self.assertEqual(compiled, 'BASE TEMPLATE')
 
