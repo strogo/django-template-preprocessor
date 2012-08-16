5cbba7a8ddec1dbc0e613459c978e76a3f34b481
Enrico <rico.bl@gmail.com>
2012-08-16 16:21:57 -0300

    Removed useless template for inheritance test.

--- src/test_project/testapp/tests/test_inheritance.py
+++ src/test_project/testapp/tests/test_inheritance.py
@@ -8,11 +8,11 @@ from test_project.helpers import compile_source
 class TestSimpleTemplateInheritance(TestCase):
     
     def test_should_expand_one_level_inheritance(self):
-        compiled = compile_source('{% extends "inheritance/level-one.html" %}').strip()
+        compiled = compile_source('{% extends "inheritance/base.html" %}').strip()
         self.assertEqual(compiled, 'BASE TEMPLATE')
 
     def test_should_expand_two_level_inheritance(self):
-        compiled = compile_source('{% extends "inheritance/level-two.html" %}').strip()
+        compiled = compile_source('{% extends "inheritance/level-one.html" %}').strip()
         self.assertEqual(compiled, 'BASE TEMPLATE')
 
     def test_should_expand_app_template_inheriting_from_template_dirs(self):
